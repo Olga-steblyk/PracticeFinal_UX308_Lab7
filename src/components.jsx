35ee85ff1a2e166aae749fa533172f647ddb0ae9
Olga-Stebko <78696791+Olga-steblyk@users.noreply.github.com>
2025-04-05 02:12:41 +0000

    added the questions

--- src/components.jsx
+++ src/components.jsx
@@ -1,4 +1,4 @@
-import {fahr2cel, hello} from './functions.js';
+import {fahr2cel, hello, sqft2acre, lawncutting, airqualityindex, yeeha, slopecal} from './functions.js';
 
 function Question1(){
     return <section>
@@ -20,4 +20,61 @@ function Question2(){
     </section>;
 }
 
-export {Question1, Question2}
+function Question3(){
+    return <section>
+Given the&nbsp; width of a lawn in metres, length of a lawn in metres and the square metres cut per minute calculate the minutes it would take to mow that lawn. Test at least 3 lawn sizes
+<h2>Results</h2>
+<p> sqft2acre(32) == "{sqft2acre(32)}"</p>
+<p> sqft2acre(43560) == "{sqft2acre(43560)}"</p>
+<p> sqft2acre(87120) == "{sqft2acre(87120)}"</p>
+    </section>;
+}
+
+function Question4(){
+    return <section>
+4. Compute the air quality given an air quality index:
+<h2>Results</h2>
+<p> lawncutting(32,11,0) == "{lawncutting(32,11,2)}"</p>
+<p> lawncutting(32,11,0) == "{lawncutting(32,11,4)}"</p>
+<p> lawncutting(32,11,0) == "{lawncutting(32,11,3)}"</p>
+    </section>;
+}
+
+function Question5(){
+    return <section>
+4. Compute the air quality given an air quality index:
+<h2>Results of Air Quality Index</h2>
+<p> airqualityindex(40) == "{airqualityindex(40)}"</p>
+<p> airqualityindex(70) == "{airqualityindex(50)}"</p>
+<p> airqualityindex(25) == "{airqualityindex(124)}"</p>
+<p> airqualityindex(40) == "{airqualityindex(178)}"</p>
+<p> airqualityindex(70) == "{airqualityindex(214)}"</p>
+<p> airqualityindex(25) == "{airqualityindex(390)}"</p>
+    </section>;
+}
+
+function Question6(){
+    return <section>
+5. yee_ha takes an integer parameter and returns a string
+<h2>Results of the Yeeha</h2>
+<p> yeeha(21) == "{yeeha(21)}"</p>
+<p> yeeha(27) == "{yeeha(27)}"</p>
+<p> yeeha(14) == "{yeeha(14)}"</p>
+<p> yeeha(10) == "{yeeha(10)}"</p>
+    </section>;
+}
+
+function Question7(){
+    return <section>
+5. yee_ha takes an integer parameter and returns a string
+<h2>Results of the Yeeha</h2>
+<p> slopecal(1,2,3,4) == "{slopecal(1,2,3,4)}"</p>
+<p> slopecal(2,2,6,8) == "{slopecal(2,2,6,8)}"</p>
+<p> slopecal(5,6,8,9) == "{slopecal(5,6,6,9)}"</p>
+<p> slopecal(3,5,7,9) == "{slopecal(3,5,7,9)}"</p>
+    </section>;
+}
+
+
+
+export {Question1, Question2, Question3, Question4, Question5, Question6, Question7}
--- src/functions.js
+++ src/functions.js
@@ -7,4 +7,71 @@ function fahr2cel (fahr){
     console.log(cel);
     return cel;
 }
-export {hello, fahr2cel}
+
+function sqft2acre(sqft){
+    
+    let acre = sqft / 43560;
+    console.log("acre");
+    return acre;
+
+}
+function lawncutting( width, length, sqm){
+    let area = width * length;
+    let min = area / sqm;
+    return min;
+}
+
+function airqualityindex(airqual){
+    var results;
+    if (airqual <50){
+        console.log(`Your input of ${airqual} is Good`);
+        return results= `Your input of ${airqual} is Good`;
+    }else if (airqual >=50 && airqual <= 100){
+        console.log(`Your input of ${airqual} is Moderate`);
+        return results= `Your input of ${airqual} is Moderate`;
+    }else if (airqual >=101 && airqual <=150){
+        console.log(`Your input of ${airqual} is Unhealthy for Sensitive Groups`);
+        return results= `Your input of ${airqual} is Unhealthy for Sensitive Groups`;
+    }else if (airqual >=151 && airqual <=200){
+        console.log(`Your input of ${airqual} is Unhealthy`);
+        return results= `Your input of ${airqual} is Unhealthy`;
+    }else if (airqual >= 201 && airqual <=300){
+        console.log(`Your input of ${airqual} is Very Unhealthy`);
+        return results= `Your input of ${airqual} is Unhealthy`;
+    }else if (airqual >300){
+        console.log(`Your input of ${airqual} is Hazardous`);
+        return results= `Your input of ${airqual} is Hazardous`;
+    }else {
+        return results= "Your input is invalid please try again";
+    }
+    return; 
+    // end of function
+    }
+
+    function yeeha (num){
+        let response;
+
+        if (num % 7 == 0  && num % 3 == 0 ){
+           console.log("Yee Ha");
+           return response = `Yee Ha`;
+        }else if (num % 3 == 0) {
+            console.log("Yee");
+            return response = `Yee`;
+        }else if ( num % 7 ==0){
+            console.log("Ha");
+            return response = `Ha`;
+        }else {
+            console.log("Nada");
+            return response = `Nada`;
+       
+        }
+       }
+
+    function slopecal( x1, y1, x2, y2){
+
+    let slp = (y2 - y1) / (x2 - x1);
+    console.log(`${slp}`);
+    return slp;
+}
+       
+export {hello, fahr2cel, sqft2acre, lawncutting, airqualityindex, yeeha, slopecal}
